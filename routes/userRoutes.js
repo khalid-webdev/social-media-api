8b487aa5a7073b491b1b4cef2a866ed4e7a0275f
khalid-webdev <khaleduzzaman67@gmail.com>
2026-03-25 06:30:09 +0600

    updating user login api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,56 +1,76 @@
-require("dotenv").config({quiet:true});
+require("dotenv").config({ quiet: true });
 const express = require("express");
 const User = require("../models/userModels");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const router =express.Router();
+const router = express.Router();
 
-router.post("/",async(req,res)=>{
-  const {username,email,password}=req.body;
-  if(!username||!email||!password){
-    return res.status(400).json({message:"missing required form fields!!!",success:false})
+router.post("/", async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "missing required form fields!!!", success: false });
   }
   const user = await User.findOne({
-    $or:[{username:username},{email:email}]
+    $or: [{ username: username }, { email: email }],
   });
-  if(user){
-    return res.status(400).json({message:user.username ===username?"Username is already taken!":"Email is already registered!",success:false});
+  if (user) {
+    return res
+      .status(400)
+      .json({
+        message:
+          user.username === username
+            ? "Username is already taken!"
+            : "Email is already registered!",
+        success: false,
+      });
   }
-  const hashedPass=await bcrypt.hash(password,10);
+  const hashedPass = await bcrypt.hash(password, 10);
   const newUser = new User({
     username,
     email,
-    password:hashedPass
+    password: hashedPass,
   });
   await newUser.save();
-  const token=generateToken({_id:newUser._id,username:newUser.username});
+  const token = generateToken({ _id: newUser._id, username: newUser.username });
   res.status(201).json(token);
-})
+});
 
 //user login api
-router.post("/login",async(req,res)=>{
+router.post("/login", async (req, res) => {
   //find the user
-  const {username,password}=req.body;
-  if(!username||!password)return res.status(400).json({message:"Missing required form fields!!!",success:false})
+  const { username, password } = req.body;
+  if (!username || !password)
+    return res
+      .status(400)
+      .json({ message: "Missing required form fields!!!", success: false });
 
   let inputEmail;
-  if(username.endsWith(".com")){
-    inputEmail=username;
+  if (username.endsWith(".com")) {
+    inputEmail = username;
   }
-    const user = await User.findOne({
-      $or:[{username:username},{email:inputEmail}]
-    });
-    if(!user)return res.status(404).json({message:'Invalid credentials',success:false})
+  const user = await User.findOne({
+    $or: [{ username: username }, { email: inputEmail }],
+  });
+  if (!user)
+    return res
+      .status(400)
+      .json({ message: "Invalid credentials", success: false });
 
-    res.json(user)
+  //compare password with bcrypt
 
-})
-
+  const comparedPass = await bcrypt.compare(password,user.password);
+  console.log(comparedPass);
+  if(!comparedPass)return res.status(400).json({ message: "Invalid credentials", success: false });
+  const token = generateToken({_id:user._id,username:user.username})
+  res.json(token);
+});
 
 //*==> common functions
-const generateToken = (data)=>{
-  return jwt.sign(data,process.env.JWT_KEY)
-}
+const generateToken = (data) => {
+  return jwt.sign(data, process.env.JWT_KEY);
+};
 
 
 module.exports = router;
