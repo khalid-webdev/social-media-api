422f916d8e100866eede45f4eac15fb9046cdef9
khalid-webdev <khaleduzzaman67@gmail.com>
2026-03-27 16:05:53 +0600

    adding smtp mail

--- config/smtp.js
+++ config/smtp.js
@@ -2,11 +2,24 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host:process.env.SMTP_HOST,
   port:process.env.SMTP_PORT,
-  secure:false,
+  secure:true,//true for 465, and false for other ports
   auth:{
     user:process.env.SMTP_USER,
     pass:process.env.SMTP_PASS
   }
 });
-
-
+const sendSMTPEmail =async(to,subject,text)=>{
+  try {
+    const mailOptions = {
+      from:process.env.SMTP_USER,
+      to,
+      subject,
+      text
+    }
+    const response= await transporter.sendMail(mailOptions);
+    console.log("Email send successfully! Message ID:",response.messageId);
+  } catch (error) {
+    console.log("Error in sending email:",error)
+  }
+}
+module.exports = sendSMTPEmail;
